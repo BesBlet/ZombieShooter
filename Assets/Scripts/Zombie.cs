b7c8f1f19edbdf75950003c205a2cf6b2fd58d9d
Kemal <45937663+BesBlet@users.noreply.github.com>
2020-12-17 20:59:04 +0300

    Alpha 0.1.5

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float moveRadius = 10;
+    public float attackRarius = 5;
+
+    Player player;
+
+    ZombieState activeState;
+
+    Animator animator;
+
+    ZombieMovement movement;
+
+   enum ZombieState
+    {
+        STAND,
+        MOVE,
+        ATTACK
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<ZombieMovement>();
+    }
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        activeState = ZombieState.STAND;
+    }
+
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        switch (activeState)
+        {
+            case ZombieState.STAND:
+                if (distance < moveRadius)
+                {
+                    activeState = ZombieState.MOVE;
+                    return;
+                }
+                movement.enabled = false;
+                //Stand
+                break;
+            case ZombieState.MOVE:
+                if(distance < attackRarius)
+                {
+                    activeState = ZombieState.ATTACK;
+                    return;
+                }
+                movement.enabled = true;
+                //Move
+                break;
+            case ZombieState.ATTACK:
+                if (distance > attackRarius)
+                {
+                    activeState = ZombieState.MOVE;
+                    return;
+                }
+                animator.SetTrigger("Shoot");
+                movement.enabled = false;
+                //Attack
+                break;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, moveRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRarius);
+
+    }
+}
---
+++ Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    Player player;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private Vector3 Position()
+    {
+        Vector3 playerPosition = player.transform.position;
+        Vector3 zombiePosition = transform.position;
+        Vector3 direction = zombiePosition - playerPosition;
+        return direction;
+    }
+
+    private void Move()
+    {
+        Vector3 direction = Position();
+
+        if (direction.magnitude > 1)
+        {
+            direction = direction.normalized;
+        }
+
+        rb.velocity = -direction * speed;
+
+        animator.SetFloat("Speed", direction.magnitude);
+
+    }
+
+    void Rotate()
+    {
+        Vector3 direction = Position();
+
+        direction.z = 0;
+        transform.up = direction;
+
+    }
+
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
